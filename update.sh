371a1f0ca8c73cef1db6ceb06cfcd34354b9c178
d-ones <dunjones@gmail.com>
2024-04-27 22:03:54 -0400

    readd content type

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo $cf_api
-echo $cf_zone
-
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$cf_zone/purge_cache" \
      -H "Authorization: Bearer $cf_api" \
+     -H "Content-Type: application/json" \
      --data "{  \"purge_everything\": true }"
