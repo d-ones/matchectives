286c213169860c2069a04f23e28bea88a623ac23
d-ones <dunjones@gmail.com>
2024-04-27 21:56:22 -0400

    reversed

--- update.sh
+++ update.sh
@@ -3,6 +3,6 @@
 echo $cf_api
 echo $cf_zone
 
-curl -X POST "https://api.cloudflare.com/client/v4/zones/$cf_api/purge_cache" \
-     -H "Authorization: Bearer $cf_zone" \
+curl -X POST "https://api.cloudflare.com/client/v4/zones/$cf_zone/purge_cache" \
+     -H "Authorization: Bearer $cf_api" \
      --data "{  \"purge_everything\": true }"
