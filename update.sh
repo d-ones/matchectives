ed073b27afb201fc8356a6ca68354ffa484644df
d-ones <dunjones@gmail.com>
2024-04-27 21:55:57 -0400

    ech test

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo $cf_api
+echo $cf_zone
+
 curl -X POST "https://api.cloudflare.com/client/v4/zones/$cf_api/purge_cache" \
      -H "Authorization: Bearer $cf_zone" \
      --data "{  \"purge_everything\": true }"
